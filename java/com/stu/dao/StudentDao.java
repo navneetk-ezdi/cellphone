e3db18731780e37b777b4162d8ee11f586e4633d
Navneet Kishore <EZDI\navneet.k@navneetk.ezdi.local>
2016-07-27 22:14:04 +0530

    removed unnecessary java dir

--- java/com/stu/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.stu.dao;
-
-import com.stu.model.Student;
-
-public interface StudentDao {
-    public int save(Student student);
-     
- 
-}
--- java/com/stu/service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.stu.service;
-
-import com.stu.model.Student;
-
-public interface StudentService {
-    public int save(Student student);
- 
-}
